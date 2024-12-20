957bfebd5ac2b6401d299100bc90aa4c05103965
Odokaa (オドカー) <xingdaiyamondo@gmail.com>
2024-12-20 13:45:20 +0800

    all code complete but null data

--- Work/src/javascript/main.js
+++ Work/src/javascript/main.js
@@ -1,1 +1,6 @@
+const menuIcon = document.getElementById("menu-icon");
+const navLinks = document.getElementById("nav-bar");
 
+menuIcon.addEventListener("click", () => {
+  navLinks.classList.toggle("active"); // Toggle the 'active' class
+});
